96406c12482c94e4d0815fc75904e29fba1cd359
scorpiocc <616067216@qq.com>
2017-04-24 15:26:38 +0800

    提交spring security代码

---
+++ Carsale/CarSale/src/main/java/com/yin/myproject/security/controller/LoginController.java
@@ -0,0 +1,20 @@
+package com.yin.myproject.security.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class LoginController {
+	
+	@RequestMapping(value = "/loginPage", method = RequestMethod.GET)
+    public String loginPage(@RequestParam(value = "error", required = false) String error, Model model) {
+        if (error != null) {
+            return "login-failure";
+        }
+        return "login";
+    }
+	
+}
